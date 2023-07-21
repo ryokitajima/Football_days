f73db3350fab2cedde7bbd187423ab4c6eae2ed2
RyoKitajima <117265598+ryokitajima@users.noreply.github.com>
2023-07-21 09:13:03 +0000

    [7/21]コメント並び順修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
+
   has_one_attached :image
   has_many :articles, dependent: :destroy
   has_many :article_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # 与フォロー関係を通じて参照→自分がフォローしている人
@@ -24,7 +27,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit:[width, height]).processed
+      image.variant(resize_to_limit:[width, height]).processed
   end
 
   def follow(user)
@@ -38,7 +41,7 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
+
   def self.search_for(content, method)
       User.where('name LIKE ?', '%' + content + '%')
   end
