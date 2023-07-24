e0d649f1f1972bfc4b9dc1b5e324bca9567e1b0d
RyoKitajima <117265598+ryokitajima@users.noreply.github.com>
2023-07-24 15:59:29 +0000

    [マイグレーションファイル修正]

--- db/migrate/20230713084958_create_articles.rb
+++ db/migrate/20230713084958_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-
+      t.integer :user_id, null: false
       t.string :title, null: false
       t.text :body, null: false
       t.timestamps null: false
